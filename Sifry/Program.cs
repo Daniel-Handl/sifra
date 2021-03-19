229f408d8da117a6d63b1b03d79b2e310c08417b
Daniel Handl <handl.daniel.2017@skola.ssps.cz>
2021-03-19 08:57:41 +0100

    přidání projektu

---
+++ Sifry/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sifry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("text na zasifrování: ");
+            string s = Console.ReadLine();
+            string xor;
+            string caesar;
+            int ckey = 7;
+            string xkey = "PRAVDA";
+
+            Console.WriteLine(XORSifra(s, xkey));
+            Console.WriteLine(CaesarSifra(s, ckey)); 
+        }
+
+        private static string CaesarSifra(string s, int key)
+        {
+            string coded = "";
+            foreach (char ch in s)
+            {
+                if (ch == 'z' | ch == 'Z')
+                {
+                    coded += Convert.ToChar(Convert.ToInt32(ch) + key-26);
+                }
+                else
+                {
+                coded += Convert.ToChar(Convert.ToInt32(ch) + key);
+                }
+            }
+
+            return coded;
+        }
+
+        private static string XORSifra(string s, string key)
+        {
+            string coded = "";
+
+
+            return coded;
+        }
+    }
+}
---
+++ Sifry/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("Sifry")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sifry")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// komponenty COM, nastavte atribut ComVisible daného typu na hodnotu true.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("fc0307db-1272-48f4-b919-2d86d7d713d2")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+// Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize
+// pomocí zástupného znaku * takto:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
